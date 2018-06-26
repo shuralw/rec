b807f5cdc63412a958dbdbbdf9fdfb32a895e080
shuralw <wessel5991+github@gmail.com>
2018-06-26 17:00:48 +0200

    Test Commit #1

--- Rezeptewoche/Program.cs
+++ Rezeptewoche/Program.cs
@@ -17,6 +17,7 @@ namespace Rezeptewoche
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // what to do
         }
     }
 }
