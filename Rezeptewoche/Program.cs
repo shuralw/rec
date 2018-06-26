2f49bafc5df6d6d61924de4249f6f39531bfe1fa
shuralw <wessel5991+github@gmail.com>
2018-06-26 17:02:38 +0200

    next test

--- Rezeptewoche/Program.cs
+++ Rezeptewoche/Program.cs
@@ -17,7 +17,7 @@ namespace Rezeptewoche
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // what to do
+            // what to do now
         }
     }
 }
